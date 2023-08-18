5c368d472fc2b3bb30a2afbcdd35e78907efda29
ritonun <jeremy.moreau2001@gmail.com>
2023-08-18 23:27:16 +0200

    update icon

--- aphe/tk-app.py
+++ aphe/tk-app.py
@@ -6,6 +6,8 @@ class App(tk.Tk):
         super().__init__(*args, **kwargs)
         self.title("Apheleia Inventory System")
         self.geometry("700x500")
+        icon = tk.PhotoImage(file="../assets/image.png")
+        self.iconphoto(False, icon)
 
 
 if __name__ == '__main__':
