fa3a8faea3eec3d4407df0761bcbc65559c02a53
ritonun <jeremy.moreau2001@gmail.com>
2023-08-19 13:32:20 +0200

    start work on gui

--- aphe/settings.py
+++ aphe/settings.py
@@ -1,4 +1,5 @@
-import os
+import os.path
 
+icon_path = os.path.join("..", "assets", "icon.png")
 db_path = os.path.join("..", "assets", "inventory.db")
 log_file_path = os.path.join("..", "assets", "log_file.log")
--- aphe/tk-app.py
+++ aphe/tk-app.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from settings import icon_path
 
 
 class App(tk.Tk):
@@ -6,8 +8,21 @@ class App(tk.Tk):
         super().__init__(*args, **kwargs)
         self.title("Apheleia Inventory System")
         self.geometry("700x500")
-        icon = tk.PhotoImage(file="../assets/image.png")
+        icon = tk.PhotoImage(file=icon_path)
         self.iconphoto(False, icon)
+
+        self.title_bar = TitleBar()
+
+
+class TitleBar(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.place(x=0, y=0, relwidth=1, relheight=0.2)
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        label = ttk.Label(self, text="Apheleia").grid(row=0, column=0)
 
 
 if __name__ == '__main__':
